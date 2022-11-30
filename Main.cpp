0018125b2a1257bdbe1fad38d72cf1e74880bf04
RileySorensen <98629467+RileySorensen@users.noreply.github.com>
2022-11-30 16:38:38 -0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include "Renderer.h"
+#include <iostream>
+
+int main(int, char**)
+{
+	std::cout << "Hello World" << std::endl;
+
+
+	Renderer renderer;
+	renderer.Initialize();
+	renderer.CreateWindow(400, 300);
+
+	bool quit = false;
+
+	while (!quit)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+	}
+	renderer.Shutdown();
+
+	return 0;
+}
